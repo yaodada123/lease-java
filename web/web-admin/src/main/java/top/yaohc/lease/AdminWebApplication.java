085ac7ef56341c8d681aa1a04091a291bc2d105f
yhc <2580577036@qq.com>
2024-10-06 15:56:26 +0800

    chore: pro init config

---
+++ web/web-admin/src/main/java/top/yaohc/lease/AdminWebApplication.java
@@ -0,0 +1,20 @@
+package top.yaohc.lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: AdminWebApplication
+ * Description:
+ *
+ * @Author 所谓独醉
+ * @Create 2024/10/6 下午3:50
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class AdminWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminWebApplication.class, args);
+    }
+}
+
